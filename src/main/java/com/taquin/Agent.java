4d0ae2b998d80136aa41943449d515d45e056a44
SimonGaufreteau <simon.gaufreteau@gmail.com>
2021-12-03 16:27:00 +0100

    Changes Agent to threads

--- src/main/java/com/taquin/Agent.java
+++ src/main/java/com/taquin/Agent.java
@@ -1,24 +1,25 @@
 package com.taquin;
 
-public class Agent {
+public class Agent extends Thread{
 
 	private final int ID;
 	private Puzzle puzzle;
 
-
-
 	public Agent(int i, Puzzle puzzle) {
+		super();
 		this.ID = i;
 		this.puzzle = puzzle;
 	}
 
+	@Override
 	public void run() {
-		while(true) {
-			//Listen to messages (check messages pile)
-
-			//Move -- synchronise moving action and tile checking
-
-			//Send messages if needed
+		while(!puzzle.isFinished()) {
+			System.out.println("Agent "+ID+" is doing something");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
--- src/main/java/com/taquin/Main.java
+++ src/main/java/com/taquin/Main.java
@@ -2,7 +2,8 @@ package com.taquin;
 
 public class Main {
     public static void main(String[] args) {
-        Puzzle puzzle = new Puzzle(24,5,5);
+        Puzzle puzzle = new Puzzle(6,5,5);
         System.out.println(puzzle);
+        puzzle.runResolution();
     }
 }
