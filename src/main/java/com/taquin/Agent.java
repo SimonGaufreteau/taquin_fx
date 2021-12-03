882142362eeb4e8574ab04f127eb4d17524e4f6b
SimonGaufreteau <simon.gaufreteau@gmail.com>
2021-12-03 17:01:41 +0100

    Adds basic javafx app

--- src/main/java/com/taquin/Agent.java
+++ src/main/java/com/taquin/Agent.java
@@ -37,4 +37,69 @@ public class Agent extends Thread{
 	public int getID() {
 		return this.ID;
 	}
+
+	public void move() {
+		Direction[] directions = getBestDirections();
+		Pair<Integer,Integer> currentPos = puzzle.getAgentPos().get(this);
+		Agent[][] currentGrid = puzzle.getCurrentGrid();
+		int x = currentPos.getKey();
+		int y = currentPos.getValue();
+		boolean moved = false;
+
+		moved = switch(directions[0]) {
+			case TOP -> y - 1 >= 0 && currentGrid[y - 1][x] == null && puzzle.moveAgent(this, Direction.TOP);
+
+			case RIGHT -> x + 1 < currentGrid[0].length && currentGrid[y][x + 1] == null && puzzle.moveAgent(this, Direction.RIGHT);
+
+			case BOTTOM -> y + 1 < currentGrid.length && currentGrid[y + 1][x] == null && puzzle.moveAgent(this, Direction.BOTTOM);
+
+			case LEFT -> x - 1 >= 0 && currentGrid[y][x - 1] == null && puzzle.moveAgent(this, Direction.LEFT);
+		};
+
+		if(!moved){
+			switch(directions[1]) {
+				case TOP -> y - 1 >= 0 && currentGrid[y - 1][x] == null && puzzle.moveAgent(this, Direction.TOP);
+
+				case RIGHT -> x + 1 < currentGrid[0].length && currentGrid[y][x + 1] == null && puzzle.moveAgent(this, Direction.RIGHT);
+
+				case BOTTOM -> y + 1 < currentGrid.length && currentGrid[y + 1][x] == null && puzzle.moveAgent(this, Direction.BOTTOM);
+
+				case LEFT -> x - 1 >= 0 && currentGrid[y][x - 1] == null && puzzle.moveAgent(this, Direction.LEFT);
+			};
+		}
+	}
+
+	/**
+	 * @return An array of size 2 => The best directions to take on X and Y (ex: [TOP, LEFT] )
+	 */
+	private Direction[] getBestDirections() {
+		Direction[] directions = new Direction[2];
+
+		int distX = getDistance(Axes.X);
+		int distY = getDistance(Axes.Y);
+
+		if(Math.abs(distX)>Math.abs(distY)){
+			directions[0] = distX>0?Direction.RIGHT:Direction.LEFT;
+			directions[1] = distY>0?Direction.TOP:Direction.BOTTOM;
+		}
+		else {
+			directions[0] = distY>0?Direction.TOP:Direction.BOTTOM;
+			directions[1] = distX>0?Direction.RIGHT:Direction.LEFT;
+		}
+
+		return directions;
+	}
+
+	private int getDistance(Axes x) {
+		Pair<Integer,Integer> currentPos = puzzle.getAgentPos().get(this);
+		Pair<Integer,Integer> destinationPos = puzzle.getAgentPos().get(this);
+		if(x.equals(Axes.X)){
+			return destinationPos.getKey()-currentPos.getKey();
+		}
+		return destinationPos.getValue()-currentPos.getValue();
+
+	}
+
+
+	private enum Axes {X,Y}
 }
--- src/main/java/com/taquin_fx/HelloApplication.java
+++ src/main/java/com/taquin_fx/HelloApplication.java
@@ -3,21 +3,45 @@ package com.taquin_fx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.shape.*;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-	@Override
-	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
-		stage.setScene(scene);
-		stage.show();
-	}
 
 	public static void main(String[] args) {
 		launch();
 	}
+
+	@Override
+	public void start(Stage stage) throws IOException {
+		//FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+		//Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+		//stage.setTitle("Hello!");
+		//stage.setScene(scene);
+		//.show();
+
+		Rectangle r1 = new Rectangle();
+		r1.setX(200);
+		r1.setY(200);
+		r1.setWidth(200);
+		r1.setHeight(200);
+		r1.setFill(Color.ALICEBLUE);
+		
+		Pane pane = new Pane();
+		pane.getChildren().add(r1);
+
+		Scene sc = new Scene(pane, 1024, 800, true);
+		stage.setScene(sc);
+		stage.setTitle("TAQUIN très TAQUIN");
+
+		stage.show();
+
+
+	}
+
+
 }
